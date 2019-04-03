74d9c96362d1a0c93b76759cbec00c13159db9e9
Jungtaek Lim (HeartSaVioR) <kabhwan@gmail.com>
2019-04-03 15:21:49 +0900

    Fix bug on new test (order matters), documented new feature

--- src/test/scala/org/apache/spark/sql/state/StateInformationInCheckpointSuite.scala
+++ src/test/scala/org/apache/spark/sql/state/StateInformationInCheckpointSuite.scala
@@ -79,7 +79,7 @@ class StateInformationInCheckpointSuite
       assert(operator.opId === 0)
       assert(operator.partitions === spark.sqlContext.conf.numShufflePartitions)
       // NOTE: this verification couples with implementation details of streaming join
-      assert(operator.storeNames === Seq("left-keyToNumValues", "left-keyWithIndexToValue",
+      assert(operator.storeNames.toSet === Set("left-keyToNumValues", "left-keyWithIndexToValue",
         "right-keyToNumValues", "right-keyWithIndexToValue"))
     }
   }
